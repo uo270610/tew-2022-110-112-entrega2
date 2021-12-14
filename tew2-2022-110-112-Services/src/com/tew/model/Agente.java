5a7603e3d75c88ea04ef13b243c8b8422b59412e
UO271883 <79576586+UO271883@users.noreply.github.com>
2021-12-14 12:27:33 +0100

    TEW-2022-110-112-7.2-Servicios: Anotación del DTO

--- tew2-2022-110-112-Services/src/com/tew/model/Agente.java
+++ tew2-2022-110-112-Services/src/com/tew/model/Agente.java
@@ -1,25 +1,40 @@
 package com.tew.model;
 
-public class Agente {
+import java.io.Serializable;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name = "agente")
+public class Agente implements Serializable{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private Long id;
 	private String login;
 	private String passwd;
 	
+	
+	@XmlElement
 	public Long getId() {
 		return id;
 	}
-	public void setId(Long id) {
-		this.id = id;
-	}
-
+	@XmlElement
 	public String getLogin() {
 		return login;
 	}
-	public void setLogin(String login) {
-		this.login = login;
-	}
+	@XmlElement
 	public String getPasswd() {
 		return passwd;
+	}
+	
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public void setLogin(String login) {
+		this.login = login;
 	}
 	public void setPasswd(String passwd) {
 		this.passwd = passwd;
--- tew2-2022-110-112-Services/src/com/tew/model/Piso.java
+++ tew2-2022-110-112-Services/src/com/tew/model/Piso.java
@@ -1,6 +1,17 @@
 package com.tew.model;
 
-public class Piso {
+import java.io.Serializable;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+
+@XmlRootElement(name = "piso")
+public class Piso implements Serializable{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private Long id;
 	private Long idAgente;
 	private double precio;
@@ -8,47 +19,65 @@ public class Piso {
 	private int estado;
 	private String ciudad;
 	private int ano;
+	private String foto;
 	
+	
+	@XmlElement
 	public int getAno() {
 		return ano;
 	}
-	public void setAno(int ano) {
-		this.ano = ano;
-	}
+	@XmlElement
 	public String getDireccion() {
 		return direccion;
 	}
-	public void setDireccion(String direccion) {
-		this.direccion = direccion;
-	}
+	@XmlElement
 	public Long getId() {
 		return id;
+	}
+	@XmlElement
+	public Long getIdAgente() {
+		return idAgente;
+	}
+	@XmlElement
+	public double getPrecio() {
+		return precio;
+	}
+	@XmlElement
+	public int getEstado() {
+		return estado;
+	}
+	@XmlElement
+	public String getCiudad() {
+		return ciudad;
+	}
+	@XmlElement
+	public String getFoto() {
+		return foto;
+	}
+	
+	
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
 	}
 	public void setId(Long id) {
 		this.id = id;
 	}
-	public Long getIdAgente() {
-		return idAgente;
+	public void setAno(int ano) {
+		this.ano = ano;
 	}
 	public void setIdAgente(Long idAgente) {
 		this.idAgente = idAgente;
 	}
-	public double getPrecio() {
-		return precio;
-	}
 	public void setPrecio(double precio) {
 		this.precio = precio;
-	}
-	public int getEstado() {
-		return estado;
 	}
 	public void setEstado(int estado) {
 		this.estado = estado;
 	}
-	public String getCiudad() {
-		return ciudad;
-	}
 	public void setCiudad(String ciudad) {
 		this.ciudad = ciudad;
+	}
+	public void setFoto(String foto) {
+		this.foto = foto;
 	}
 }
