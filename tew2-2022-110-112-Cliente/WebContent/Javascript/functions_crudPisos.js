455850100433b737b3d591f218fc2d1e19a3d390
omega <omega@DESKTOP-UK941MF>
2021-12-15 23:53:28 +0100

    alta pisos

--- tew2-2022-110-112-Cliente/WebContent/Javascript/functions_crudPisos.js
+++ tew2-2022-110-112-Cliente/WebContent/Javascript/functions_crudPisos.js
@@ -1,3 +1,66 @@
 /**
  * 
  */
+
+
+function Model() {
+	//variable Piso.
+	 this.Piso= null;
+	 
+
+	this.rellenaPisoDelFormulario = function () {
+		var piso= {
+		id=1,
+		idAgente=localStorage.getItem('agente'),
+		precio=$("#precio").val(),
+		direccion=$("#direccion").val(),
+		ciudad=$("#ciudad").val(),
+		anyo=$("#ano").val(),
+		estado=$("#estado").val(),
+		foto=$("#foto").val()
+		}
+		return piso
+	        }
+	
+	this.AltaPiso = function(Piso){
+		PisosServicesRs.savePiso({
+			$entity:Piso,
+			$contentType:"application/json"
+		});
+	}
+	
+	this.EditarPiso=function(Piso){
+		PisosServicesRs.updatePiso({
+			$entity:Piso,
+			$contentType:"application/json"
+		});
+	}
+	        
+	  
+
+};
+
+
+function Controller(varmodel) {
+	var that = this;
+	this.model = varmodel;
+	this.init = function() {
+		$("#btnSave").click(
+				function(event){
+		    Piso=that.model.rellenaPisoDelFormulario();
+		    that.model.AltaPiso(Piso);
+		    	window.location.href="listado.html";
+		    	return false;
+				
+		});
+}
+			
+};	
+$(function() {
+	// Creamos el modelo con los datos y la conexión al servicio web.
+	var model = new Model();
+	// Creamos el controlador
+	var control = new Controller(model);
+	// Iniciamos la aplicación
+	control.init();
+});
