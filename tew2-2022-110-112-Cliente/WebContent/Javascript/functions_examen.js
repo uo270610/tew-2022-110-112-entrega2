5d7d2cbd520af7ef41f3dd41fb94bcd7a2ce2a51
Uo271883 <Uo271883@DESKTOP-A9MB9D4.mshome.net>
2021-12-16 18:36:25 +0100

    funciones

--- tew2-2022-110-112-Cliente/WebContent/Javascript/functions_examen.js
+++ tew2-2022-110-112-Cliente/WebContent/Javascript/functions_examen.js
@@ -5,15 +5,36 @@
 function Model() {
 	//variable Piso.
 	 this.piso= null;
-	 this.IdArray = [];
+	 this.precioArray = [];
 	 this.tbPisos = null;
-	 
+	 this.nmax = null;
+	 this.nmin = null;
 	 this.load = function() {
 		this.tbPisos = PisosServicesRs.getPisos();
 	 }
+	 this.precios = function(){
+		 this.n = 0
+		 this.load();
+		 for(i in tbPisos){
+			 precioArray[n] = this.tbPisos[i]["precio"];
+			 n=n+1;
+		 }
+	 }
+	 this.max = function(){
+		 this.nmax = Math.max(...precioArray);
+	 }
+	 this.max = function(){
+		 this.nmin = Math.min(...precioArray);
+	 }
 }
 function Controller(varmodel,varview) {
-	
+	var that = this;
+	this.model = varmodel;
+	this.view=varview;
+	this.init = function() {
+		$("#umbralSuperior").val(that.model.max());
+		$("#umbralInferior").val(that.model.min());
+	}
 }
 
 function View(){
